a6bc177f5f641d0ec87c0a09233f602815e23c73
Juan E Afable <juanjuanzerojuan@gmail.com>
2019-01-29 20:26:36 -0500

    Initial Commit, started a flow monitoring inspection tracking system for user.

---
+++ FlowMonitoringInsp/FlowMonitoringInsp/Models/FlowMeterModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace FlowMonitoringInsp.Models
+{
+    public class FlowMeterModel
+    {
+        public enum FM_model { Hach, ISCO, Telog, Other}
+
+        [Key]
+        public int FlowMeterModelID { get; set; }
+        public FM_model FMModel { get; set; }
+        public int SerialNumber { get; set; }
+    }
+}
